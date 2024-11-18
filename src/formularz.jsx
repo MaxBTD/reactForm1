1c40819aa17704469a8dbf0866149e2c512f8214
MaxBTD <97893081+MaxBTD@users.noreply.github.com>
2024-11-18 15:05:46 +0100

    finished task

--- src/formularz.jsx
+++ src/formularz.jsx
@@ -1,27 +1,41 @@
 import "./formularz.css";
-import { useEffect } from "react";
 import { useState } from "react";
 
 function Formularz(){
     const [isChecked, setChecked] = useState(false);
 
-    const formDownload = () =>{
+    async function formDownload(){
         let imie = document.getElementById("inputName").value;
         let nazwisko = document.getElementById("inputSurname").value;
         let email = document.getElementById("inputEmail").value;
         let haslo = document.getElementById("inputPassword").value;
         let hasloSpraw = document.getElementById("inputRepeatPassword").value;
-        
-        document.getElementById("results").innerHTML = imie;
-        
+
+        if(imie.trim().length === 0){
+           await alert("Imie nie wpisane");
+        } else if(nazwisko.trim().length === 0){
+            await alert("NNazwisko nie wpisane");
+        }else if(email.trim().length === 0){
+            await alert("E-mail nie wpisany");
+        }else if(haslo.trim().length === 0){
+            await alert("Hasło nie wpisane");
+        }else if(hasloSpraw.trim().length === 0){
+            await alert("Powtórzenie hasła nie wpisane");
+        }else{
+            if(haslo === hasloSpraw){
+                await alert("Dania poprawne");
+            }else{
+                await alert("Hasła nie zgadzają się");
+            }
+        }
     }
 
-    useEffect(() => {
+    const checkCheck = () => {
         setChecked(document.getElementById("checkboxAgreement").checked);
-    });
+    };
 
     return(
-        <div id="formularz">
+        <form id="formularz">
             <p>Imię</p>
             <input type="text" id="inputName"/>
 
@@ -39,12 +53,12 @@ function Formularz(){
 
             <div style={{display:"flex"}}>
                 <p>Zapoznałem się z <br/>regulaminem</p>
-                <input type="checkbox" id="checkboxAgreement"/>
+                <input type="checkbox" id="checkboxAgreement" onChange={() => {checkCheck()}}/>
             </div>
 
-            <div id="btnDiv"><button disabled={isChecked} id="formBtn" onClick={() => {formDownload()}}>Zatwierdź</button></div>
+            <div id="btnDiv"><button disabled={!isChecked} id="formBtn" onClick={() => {formDownload()}}>Zatwierdź</button></div>
             <p id="results"></p>
-        </div>
+        </form>
     );
 }
 
